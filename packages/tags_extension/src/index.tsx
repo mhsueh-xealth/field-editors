e8ad0ca896c24a111e1f4ec450793ea36b572e64
Martin Hsueh <martin.hsueh@xealth.io>
2020-05-28 10:17:42 -0700

    test

--- packages/tags_extension/src/index.tsx
+++ packages/tags_extension/src/index.tsx
@@ -1,3 +1,14 @@
 import { TagsEditorContainer } from './TagsEditorContainer';
+import * as React from 'react';
+import { render } from 'react-dom';
+import { init, FieldExtensionSDK } from 'contentful-ui-extensions-sdk';
+import '@contentful/forma-36-react-components/dist/styles.css';
+import '@contentful/forma-36-fcss/dist/styles.css';
+import './index.css';
 
 export const TagsEditor = TagsEditorContainer;
+
+init((sdk: FieldExtensionSDK) => {
+  sdk.window.startAutoResizer();
+  render(<TagsEditor sdk={sdk as FieldExtensionSDK} />, document.getElementById('root'));
+});
